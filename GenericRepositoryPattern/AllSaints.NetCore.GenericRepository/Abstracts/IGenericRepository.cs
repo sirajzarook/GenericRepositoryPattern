b641caa95a0c48d56efaae6d88c3a8af3d804434
sirajzarook <sirajz@gmail.com>
2017-10-17 21:55:59 +0100

    Code upload

---
+++ GenericRepositoryPattern/AllSaints.NetCore.GenericRepository/Abstracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllSaints.NetCore.GenericRepository.Abstracts
+{
+	public interface IGenericRepository<TEntity, in TKey> where TEntity : class
+	{
+		TEntity GetById(TKey id);
+		IQueryable<TEntity> GetAll();
+		bool Create(TEntity entity);
+		bool Update(TEntity entity);
+		bool Delete(TEntity entity);
+		bool Save();
+	}
+}
---
+++ GenericRepositoryPattern/AllSaints.NetCore.GenericRepository/EntityFrameworkRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllSaints.NetCore.GenericRepository
+{
+	public class EntityFrameworkRepository<TEntity, TKey> where TEntity : class
+	{
+		private readonly DbContext _dbContext;
+		public EntityFrameworkRepository(DbContext dbContext)
+		{
+			if (dbContext == null) throw new ArgumentNullException("dbContext");
+			_dbContext = dbContext;
+		}
+		protected DbContext DbContext
+		{
+			get { return _dbContext; }
+		}
+		public void Create(TEntity entity)
+		{
+			if (entity == null) throw new ArgumentNullException("entity");
+			DbContext.Set<TEntity>().Add(entity);
+
+		}
+		public TEntity GetById(TKey id)
+		{
+			return _dbContext.Set<TEntity>().Find(id);
+		}
+
+		public IQueryable<TEntity> GetAll()
+		{
+			return _dbContext.Set<TEntity>().AsQueryable();
+		}
+
+		public void Delete(TEntity entity)
+		{
+			if (entity == null) throw new ArgumentNullException("entity");
+			DbContext.Set<TEntity>().Attach(entity);
+			DbContext.Set<TEntity>().Remove(entity);
+		}
+		public void Update(TEntity entity)
+		{
+			if (entity == null) throw new ArgumentNullException("entity");
+			DbContext.Set<TEntity>().Attach(entity);
+			DbContext.Entry(entity).State = EntityState.Modified;
+
+		}
+	}
+}
